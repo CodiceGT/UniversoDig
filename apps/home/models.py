546efe23cddebe273f1c3d7d97e175c26406c7ec
ramirolv <rlopezv11@miumg.edu.gt>
2022-12-05 23:40:09 -0600

    Squashed commit of the following:
    commit 86d0ef466129e8ac663fac8de00982bc5f4c3812
    Merge: 0e3994e 4255879
    Author: ramirolv <rlopezv11@miumg.edu.gt>
    Date:   Mon Dec 5 15:32:27 2022 -0600
    
        Merge branch 'devRamiro'
    
    commit 425587984fc3851dbddcf68b71b89e1c54e90ee2
    Author: ramirolv <rlopezv11@miumg.edu.gt>
    Date:   Mon Dec 5 15:30:28 2022 -0600
    
        Config Login and Logout
    
    commit c9fea816f78a2e4ce7d7d0e901a639a81ea05afe
    Author: ramirolv <rlopezv11@miumg.edu.gt>
    Date:   Tue Nov 29 12:58:16 2022 -0600
    
        Configure to deploy
    
    commit 4d51e0697676e5e4e6872490ac9e9db4780090b7
    Author: ramirolv <rlopezv11@miumg.edu.gt>
    Date:   Tue Nov 29 11:32:08 2022 -0600
    
        Creación de la aplicación

--- apps/home/models.py
+++ apps/home/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
+from django.dispatch import receiver
 
 # Create your models here.
+
+
+
+
+
+#Modelos para manejo de usuarios
+class Cuenta(models.Model):
+    tipo = models.CharField(max_length=25)
+    usuario = models.OneToOneField(User, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.usuario.username
+
+
+@receiver(post_save, sender=User)
+def crear_cuenta(sender, instance, created, **kwargs):
+    if created:
+        Cuenta.objects.create(usuario=instance)
+
+
+@receiver(post_save, sender=User)
+def guardar_cuenta(sender, instance, created, **kwargs):
+    instance.cuenta.save()
