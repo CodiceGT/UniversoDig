68bab87fb936aa2fb12e62a383f4649179d09dbc
JO844SE <perezerik387@gmail.com>
2022-12-07 15:05:38 -0600

    Se agrego form de cliente, lista de cliente y registro de cliente

--- apps/home/views.py
+++ apps/home/views.py
@@ -1,24 +1,24 @@
-from django.contrib.auth import logout
-from django.contrib.auth.views import LoginView
-from django.contrib.auth.decorators import login_required
-from django.views import generic
-from django.shortcuts import render, redirect
+
+from django.views.generic import ListView, CreateView
+from .forms import ClienteForm
+from .models import Cliente
+from django.urls import reverse_lazy
 
 # Create your views here.
 
-#Vistas para inicio y cierre de sesión
-class LoginView(LoginView):
-    template_name = 'registration/login.html'
+# Registrar Clientes
+class CrearClienteView(CreateView):
+    template_name='FormClientes.html'
+    form_class = ClienteForm
+    success_url = reverse_lazy('cliente')
 
 
-@login_required
-def HomeView(request):
-    return render(request, 'index.html')
+#Listar clientes 
+class ListarCliente(ListView):
+    template_name = 'Cliente.html'
+    model = Cliente
+
+    def get_queryset(self):
+        return Cliente.objects.all()
 
 
-def LogoutView(request):
-    logout(request)
-    return redirect('home:login')
-
-
-#Vista para Dashboard
