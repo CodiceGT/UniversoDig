5502d37ebc4d9b2fc608d673cb3610d043168df8
ramirolv <rlopezv11@miumg.edu.gt>
2023-02-14 17:56:21 -0600

    Campo de saldo agregado a las contrataciones

--- apps/home/models.py
+++ apps/home/models.py
@@ -35,6 +35,7 @@ class Contratacion(models.Model):
     cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
     servicio = models.ForeignKey(Servicio, on_delete=models.CASCADE)
     direccion = models.CharField(max_length=128)
+    saldo = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     estado = models.CharField(max_length=1, default='d')
     # Estados: d=al dia, p=pendiente de pago, c=cancelado, 
     creacion = models.DateTimeField(auto_now=True)
