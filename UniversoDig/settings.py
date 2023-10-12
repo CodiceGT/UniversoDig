4efb2c51e10a576c58792844c708ae779af9ebd7
Ramiro López <roveltaim1973@gmail.com>
2023-10-12 16:54:38 -0600

    feat: navbar mobile first version

--- UniversoDig/settings.py
+++ UniversoDig/settings.py
@@ -38,15 +38,15 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
-    'apps.home',
-    'django_crontab',
-    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'whitenoise.runserver_nostatic',
+    'django_crontab',
+    'apps.home',
 ]
 
 MIDDLEWARE = [
